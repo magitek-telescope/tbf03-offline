031900744a63a3e192b83f0b91957484f8425e2f
Takuma Hanatani <mail@potato4d.me>
2017-10-13 21:39:06 +0900

    Update SW

--- src/service-worker.js
+++ src/service-worker.js
@@ -24,7 +24,7 @@ self.toolbox.precache(
 );
 
 // dynamically cache any other local assets
-self.toolbox.router.any('/*', self.toolbox.cacheFirst);
+self.toolbox.router.any('/*', self.toolbox.networkFirst);
 
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
