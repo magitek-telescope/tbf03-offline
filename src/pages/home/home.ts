532b3b3e4ea0f327c2ced39a6df5ba88c5bfc8cd
榊原昌彦 <sakakibara@rdlabo.jp>
2017-10-17 16:22:55 +0900

    HTTPをDIに

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,45 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import { CloudfunctionsProvider } from '../../providers';
 import { Circle, searchCircles } from '../../models/circle';
 import { getBookmark, addBookmark, removeBookmark } from '../../models/bookmarks';
 import { getLocalCircles } from '../../models/circle';
 
-@Injectable()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [CloudfunctionsProvider]
 })
 export class HomePage {
 
   public circles: Circle[] = [];
   public visibleCircles: Circle[] = [];
   public bookmarks: string[] = [];
-  private baseURL: string = 'https://us-central1-tbf03-offline.cloudfunctions.net/scraper'
 
   constructor(
     public navCtrl: NavController,
-    private http: HttpClient
-  ) {
-    this.bookmarks = getBookmark();
-    if(navigator.onLine) {
-      this.http.get(this.baseURL).subscribe((json: any) => {
-        this.circles = JSON.parse(json).list.map((circle) => {
-          if(!circle.circleCutImage) {
-            circle.circleCutImage = {url: null};
-          }
-          return circle;
-        });
-        this.visibleCircles = this.circles;
-        localStorage.setItem('circles', JSON.stringify(this.circles));
-      });
-    } else {
-      this.circles = getLocalCircles();
-      this.visibleCircles = this.circles;
-    }
+    public cf: CloudfunctionsProvider
+  ){}
+
+  ionViewDidLoad() {
+      this.bookmarks = getBookmark();
+      if(navigator.onLine) {
+          this.cf.getScraper()
+              .subscribe((json: any) => {
+              this.circles = json;
+              this.visibleCircles = this.circles;
+              localStorage.setItem('circles', JSON.stringify(this.circles));
+          });
+      } else {
+          this.circles = getLocalCircles();
+          this.visibleCircles = this.circles;
+      }
   }
 
   getItems(ev: any) {
---
+++ src/providers/index.ts
@@ -0,0 +1,1 @@
+export { CloudfunctionsProvider } from './cloudfunctions/cloudfunctions';
